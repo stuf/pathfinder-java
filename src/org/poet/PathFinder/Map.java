3978da541217e0f6597d6cba2aa74a2c54fc801f
Stefan Rimaila <stuf@nori-book.local>
2014-11-06 17:37:34 +0200

    Refined classes

--- src/org/poet/PathFinder/Map.java
+++ src/org/poet/PathFinder/Map.java
@@ -6,11 +6,34 @@ package org.poet.PathFinder;
  */
 public class Map {
   private Node[][] mapNodes;
+  private int width;
+  private int height;
 
+  /**
+   * Create a default map
+   */
   public Map () {
-    int width = 10;
-    int height = 5;
+    this.width = 10;
+    this.height = 5;
 
+    this.createMap();
+  }
+
+  /**
+   * @param width Map width
+   * @param height Map height
+   */
+  public Map (int width, int height) {
+    this.width = width;
+    this.height = height;
+
+    this.createMap();
+  }
+
+  /**
+   * Create the node matrix that represents the map
+   */
+  private void createMap () {
     this.mapNodes = new Node[height][width];
 
     for (int i = 0; i < height; i++) {
@@ -28,6 +51,22 @@ public class Map {
    */
   public Node getNode (int x, int y) {
     return this.mapNodes[y][x];
+  }
+
+  /**
+   *
+   * @return Gets the map's height (1-based)
+   */
+  public int getHeight () {
+    return this.mapNodes.length;
+  }
+
+  /**
+   *
+   * @return Gets the map's width (1-based)
+   */
+  public int getWidth () {
+    return this.mapNodes[0].length;
   }
 
   /**
